58897a18f911d5001c3ed104b4f4f4efd3327e82
jinane42 <jinane.ouzina@gmail.com>
2021-09-23 17:02:52 +0200

    CRUD, login & signup done

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,8 @@ const express = require('express');
 const bodyParser =  require('body-parser');
 const mongoose = require ('mongoose');
 
-const Thing = require('./models/Thing');
+const stuffRoute = require('./routes/stuff')
+const userRoute = require('./routes/user')
 
 const app = express();
 
@@ -26,92 +27,7 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-app.post('/api/stuff', (req, res, next) => {
-    const thing = new Thing({
-        title: req.body.title, 
-        description: req.body.description,
-        imageUrl: req.body.imageUrl,
-        price: req.body.price,
-        userId: req.body.userId,
-    });
-    thing.save().then(
-        () => {
-            res.status(201).json({
-                message: 'Post saved successfully'
-            })
-        }
-    ).catch(
-        (error) => {
-            res.status(400).json({
-                error: error
-            })
-        }
-    )
-});
-
-app.get('api/stuff/:id', (req, res, next) => {
-    Thing.findOne({
-        _id: req.params.id
-    }).then(
-        (thing) => {
-            res.status(200).json(thing);
-        }
-    ).catch(
-        (error) => {
-            res.status(404).json({
-                error: error
-            });
-        }
-    )
-});
-
-app.put('api/stuff/:id', (req, res, next) =>{
-    const thing = new Thing({
-        _id: req.params.id,
-        title: req.body.title, 
-        description: req.body.description,
-        imageUrl: req.body.imageUrl,
-        price: req.body.price,
-        userId: req.body.userId,
-    });
-    Thing.updateOne({_id: req.params.id}).then(
-        () => {
-            res.status(201).json({
-                message:'Thing updates successfully'
-            });
-        }
-    )
-})
-
-app.delete('/api/stuff/:id', (req, res, next) => {
-    Thing.deleteOne({_id: req.params.id}).then(
-      () => {
-        res.status(200).json({
-          message: 'Deleted!'
-        });
-      }
-    ).catch(
-      (error) => {
-        res.status(400).json({
-          error: error
-        });
-      }
-    );
-  });
-  
-app.use('/api/stuff',(req, res, next) => {
-    Thing.find().then(
-        (things) => {
-          res.status(200).json(things);  
-        }
-    ).catch(
-        (error) => {
-            res.status(400).json({
-                error: error
-            });
-        }
-    )
-  });
-  
+app.use('/api/stuff', stuffRoute)
+app.use('/api/auth', userRoute)
 
 module.exports = app;
